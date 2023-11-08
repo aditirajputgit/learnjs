e24ed8af01e15fa05dfb72559385273a2764b05f
Aditi Rajput <122589709+aditirajputgit@users.noreply.github.com>
2023-11-08 03:21:43 +0000

    changes of datatype

---
+++ 01_basic/01variable.js
@@ -0,0 +1,14 @@
+const accountId = 1234
+let email = "aditirajput200017@gmail.com"
+var accountNumber = 542719726519
+accountCity = "Jaipur"
+
+email = "aghyrttsg@gmail.com"
+accountNumber = 82736523728
+accountCity = "Indore"
+
+/*
+    prefer not to use var due to scope issues and in function
+*/
+
+console.table([email,accountNumber,accountCity])
---
+++ 01_basic/02variable.js
@@ -0,0 +1,21 @@
+"use strict"; // treat new version of code
+//alert("hello")
+
+// alert(3+3) // it will not work because we are using nodejs not browser
+
+
+
+//number
+//string
+//boolean
+//bigint
+//null == standard value
+//undefined =>
+//sysmbol => unique
+
+
+//object 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null)  // object ans ayega
+console.log(typeof "string")
