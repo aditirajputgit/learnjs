f16ff80bf79b6a0d32d092c467623247f07a449b
Aditi Rajput <122589709+aditirajputgit@users.noreply.github.com>
2023-11-08 03:44:46 +0000

    conversion of datatype

---
+++ 01_basic/03conversion.js
@@ -0,0 +1,31 @@
+let score = 32
+
+let totalscore = null
+
+let scores = Number(totalscore) //conversion capital format hota h first letter like Number , String 
+
+//console.log(typeof score);
+//console.log(typeof totalscore);
+//console.log(typeof scores);
+
+
+//Conversion of everything in number 
+//console.log(score); //"43" //string se number change ho jayega 
+//console.log(scores); // "32abn" // NaN ye pure number in hota h Nan is a mixture of number and string or any sysmbol 
+//console.log(score); //null // 0 ans ayega
+//console.log(score); //undefined ho // to ans m Nan a jayega
+// aur bool ka true krege to 1 ayega aur false ke liye 0 ayega
+
+
+//conversion into bool
+let check = "heloo";
+let bools = Boolean(check)
+console.log(bools);
+console.log(typeof bools);
+
+
+//conversion into string
+let checks = true
+let strings = String(checks)
+console.log(strings);
+console.log(typeof strings);
