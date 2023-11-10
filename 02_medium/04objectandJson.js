630feb1fa1f15a4f05137b7b6a677eb87be05765
Aditi Rajput <122589709+aditirajputgit@users.noreply.github.com>
2023-11-10 04:33:25 +0000

    Json and object aage aur pdege

--- 02_medium/04objectandJson.js
+++ 02_medium/04objectandJson.js
@@ -14,3 +14,23 @@ const course = {
 //2. is - const {name : n} = course // fir call kr skte h  by n mname
 
 const {name: n} = course
+console.log(n);
+
+// {} // cury bracket m jo hota h vo destructing ke liye use kiya jata h 
+//ye cheej react m jada ache se smjaya jata h 
+
+//Api in nothing like apna kamam kise pr dalna ab ky hota h phle value server se xml ke form  m ati thi aur
+//ab sari ki sari value json ke foem m ati h 
+
+//Agar json k astructure dekhe to vo esa kuch bna kr dega  - 
+/*{   name :"Js course in hindi",
+    price: 999,
+    by: "aditi",
+    timeperiod : "4 month",
+}*/
+//ye objet ki trha dikh rha but object ni h like kuki object m naam hota h aur ye json h jisme naam ni h 
+//issme dono cheeje string hoti h key an dvalue
+// Ab jaroori ni h ki humne hmesha object hi mile api m array bhi mil skta h 
+
+
+
